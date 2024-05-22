eff35706423356ff2457cc305925959f34f3fc83
Chris-to-phr <chrish.tri.ho@gmail.com>
2024-05-22 10:11:04 -0500

    WordCounter::Challenge::Completed

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿string paragraph = "Hello, hello! How are you? Are you doing well? Well, well, well.";
+Dictionary<string, int> result = WordCounter.CountWords(paragraph);
+
+foreach (var item in result)
+{
+    Console.WriteLine($"{item.Key}: {item.Value}");
+}
